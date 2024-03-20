81fdd73d49c8298c6769a50ba252e9bf20f17562
MireX420 <adrian_mirek@yahoo.no>
2024-03-20 13:43:49 +0100

    AK3 Testing

---
+++ TestAK3/components/Navbar.jsx
@@ -0,0 +1,15 @@
+export default function Navbar(){
+    return(
+        <>
+        <nav id="navList">
+            <ul>
+                <li><button id="htmlButton">HTML</button></li>
+                <li><button id="cssButton">CSS</button></li>
+                <li><button id="jsButton">JavaScript</button></li>
+                <li><button id="reactButton">React</button></li>
+                <li><button id="sanButton">Sanity and headless CMS</button></li>
+            </ul>
+        </nav>
+        </>
+    )
+}
---
+++ TestAK3/my-react-app/src/App.jsx
@@ -0,0 +1,20 @@
+import Article from '../../components/Article'
+import Navbar from '../../components/Navbar'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div id="container">
+        <Navbar />
+        <main>
+            <Article />
+        </main>
+    </div>
+    </>
+  )
+}
+
+export default App
